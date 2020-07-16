96612c3fe850fcb024a7f541ca5197eedfac6fb4
ednaldomarinho <ednaldosmarinho@gmail.com>
2020-07-16 12:58:44 -0300

    Lesson 12 - Redirect and View Routes

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::view('/view', 'welcome');
+
+// Route::get('/view', function () {
+//     return view('welcome');
+// });
+
+Route::redirect('/redirect1', 'redirect2');
+
+Route::get('redirect2', function () {
+     return "<h1>Redirect 2</h1>";
+ });
+
+// Route::get('redirect1', function () {
+//     return redirect('/redirect2');
+// });
 
 Route::get('/produtos/{idProduct?}', function ($idProduct = '') {
     return "<h1>Produto: {$idProduct}</h1>";
