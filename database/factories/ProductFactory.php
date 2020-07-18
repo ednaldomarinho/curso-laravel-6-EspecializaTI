bd960dcfde9ce602bea14ea713b8f09caabb2913
ednaldomarinho <ednaldosmarinho@gmail.com>
2020-07-18 15:56:06 -0300

    Lesson 42 - Challenge: Create Product Factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,11 +2,13 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Models\User;
+use App\Models\Product;
 use Faker\Generator as Faker;
 
-$factory->define(User::class, function (Faker $faker) {
+$factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        'name'=>$faker->unique()->word,
+        'description'=>$faker->sentence(),
+        'price'=>12.2
     ];
 });
